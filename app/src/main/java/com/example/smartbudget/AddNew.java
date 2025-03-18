89280c7d3b8999dd0fb7e6a6938ccb6145151f32
lukas-havlicek-krizik <lukas.havlicek@skolakrizik.cz>
2025-03-18 22:00:37 +0100

    "AddNew" screen - spinner options.

--- app/src/main/java/com/example/smartbudget/AddNew.java
+++ app/src/main/java/com/example/smartbudget/AddNew.java
@@ -3,6 +3,8 @@ package com.example.smartbudget;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class AddNew extends AppCompatActivity {
 
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +25,12 @@ public class AddNew extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        spinner = findViewById(R.id.kategorie);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinnerKategoriePrijem, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
     public void changeScreen(View view){
         Intent intentMain = new Intent(AddNew.this, MainActivity.class);
