0c9869ffececfbbe6c054b2b99088eb96af85dc2
lukas-havlicek-krizik <lukas.havlicek@skolakrizik.cz>
2025-03-22 10:23:18 +0100

    "Overview" - listing database

--- app/src/main/java/com/example/smartbudget/Overview.java
+++ app/src/main/java/com/example/smartbudget/Overview.java
@@ -3,6 +3,8 @@ package com.example.smartbudget;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,22 +12,48 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Overview extends AppCompatActivity {
+import java.util.List;
 
+public class Overview extends AppCompatActivity {
+    private ZaznamOperations zaznamDBoperations;
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_overview);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        listView = findViewById(R.id.listView);
+
+
+        zaznamDBoperations = new ZaznamOperations(this);
+        zaznamDBoperations.open();
+
+        List<Zaznam> values = zaznamDBoperations.getAllZaznamy();
+
+        ArrayAdapter<Zaznam> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, values);
+        listView.setAdapter(adapter);
+
     }
 
     public void changeScreen(View view){
         Intent intentMain = new Intent(Overview.this, MainActivity.class);
         startActivity(intentMain);
+    }
+    @Override
+    protected void onResume() {
+        zaznamDBoperations.open();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        zaznamDBoperations.close();
+        super.onPause();
     }
 }
