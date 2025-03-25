c40f8ba66a99a186eae05ac7f6d9e6e9eb477a24
lukas-havlicek-krizik <lukas.havlicek@skolakrizik.cz>
2025-03-25 16:22:45 +0100

    Preset input fields from preferences.

--- app/src/main/java/com/example/smartbudget/Limits.java
+++ app/src/main/java/com/example/smartbudget/Limits.java
@@ -1,8 +1,10 @@
 package com.example.smartbudget;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +13,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Limits extends AppCompatActivity {
-
+    SharedPreferences sp;
+    private String zbyvajiciLimitPref;
+    private String nastavenyLimitPref;
+    private EditText nastavLimit;
+    private EditText zbyvaLimit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +28,16 @@ public class Limits extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        nastavLimit = findViewById(R.id.nastavlimitVstup);
+        zbyvaLimit = findViewById(R.id.zbyvalimitVstup);
+
+        sp = getSharedPreferences("limits",MODE_PRIVATE);
+        nastavenyLimitPref = sp.getString("nastavenyLimit",String.valueOf(1000));
+        zbyvajiciLimitPref = sp.getString("zbyvajiciLimit",String.valueOf(900));
+
+        nastavLimit.setText(nastavenyLimitPref);
+        zbyvaLimit.setText(zbyvajiciLimitPref);
+
     }
     public void changeScreen(View view){
         Intent intentMain = new Intent(Limits.this, MainActivity.class);
